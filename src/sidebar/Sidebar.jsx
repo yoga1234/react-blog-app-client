be810c21248e4aa771323cf65723c3935a0298eb
Septiana Yoga <septianayoga30@gmail.com>
2022-10-26 14:27:21 +0700

    sidebar styling

--- src/sidebar/Sidebar.jsx
+++ src/sidebar/Sidebar.jsx
@@ -1,5 +1,39 @@
 import "./Sidebar.css";
 
 export default function Sidebar() {
-  return <div className="sidebar">Sidebar</div>;
+  return (
+    <div className="sidebar">
+      <div className="sidebarItem">
+        <span className="sidebarTitle">ABOUT ME</span>
+        <img
+          src="https://images.pexels.com/photos/230129/pexels-photo-230129.jpeg"
+          alt=""
+        />
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea incidunt
+          sapiente voluptas, cum repudiandae fugit.
+        </p>
+      </div>
+      <div className="sidebarItem">
+        <span className="sidebarTitle">CATEGORIES</span>
+        <ul className="sidebarList">
+          <li className="sidebarListItem">Life</li>
+          <li className="sidebarListItem">Music</li>
+          <li className="sidebarListItem">Style</li>
+          <li className="sidebarListItem">Sport</li>
+          <li className="sidebarListItem">Tech</li>
+          <li className="sidebarListItem">Cinema</li>
+        </ul>
+      </div>
+      <div className="sidebarItem">
+        <span className="sidebarTitle">FOLLOW US</span>
+        <div className="sidebarSocial">
+          <i className="sidebarIcon fa-brands fa-square-facebook"></i>
+          <i className="sidebarIcon fa-brands fa-square-twitter"></i>
+          <i className="sidebarIcon fa-brands fa-square-pinterest"></i>
+          <i className="sidebarIcon fa-brands fa-square-instagram"></i>
+        </div>
+      </div>
+    </div>
+  );
 }
