51827d0831a59effd807441fde573ac72fd5a6e2
Septiana Yoga <septianayoga30@gmail.com>
2022-10-31 16:36:00 +0700

    finishing edit account styling

--- src/pages/settings/Settings.jsx
+++ src/pages/settings/Settings.jsx
@@ -4,7 +4,32 @@ import Sidebar from "../../components/sidebar/Sidebar";
 export default function Settings() {
   return (
     <div className="settings">
-      <div className="settingsWrapper"></div>
+      <div className="settingsWrapper">
+        <div className="settingsTitle">
+          <span className="settingsUpdateTitle">Update Your Account</span>
+          <span className="settingsDeleteTitle">Delete Account</span>
+        </div>
+        <form className="settingsForm">
+          <label>Profile Picture</label>
+          <div className="settingsPP">
+            <img
+              src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+              alt=""
+            />
+            <label htmlFor="fileInput">
+              <i className="settingsPPIcon far fa-user-circle"></i>
+            </label>
+            <input type="file" id="fileInput" style={{ display: "none" }} />
+          </div>
+          <label>Username</label>
+          <input type="text" placeholder="Username Here" />
+          <label>Email</label>
+          <input type="text" placeholder="Email Here" />
+          <label>Password</label>
+          <input type="password" />
+          <button className="settingsSubmit">Update</button>
+        </form>
+      </div>
       <Sidebar />
     </div>
   );
