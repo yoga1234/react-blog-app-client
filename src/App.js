41d6ceeae1952aa18657ebf59caa517fd1c55441
Septiana Yoga <septianayoga30@gmail.com>
2022-11-01 12:55:10 +0700

    initializing router dom

--- src/App.js
+++ src/App.js
@@ -3,15 +3,14 @@ import TopBar from "./components/topbar/TopBar";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
 import Settings from "./pages/settings/Settings";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 
 function App() {
   return (
     <div className="App">
       <TopBar />
-      {/* <Home /> */}
-      {/* <Single /> */}
-      {/* <Write /> */}
-      <Settings />
+      <Register />
     </div>
   );
 }
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,5 +1,25 @@
 import "./Login.css";
 
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <div className="login">
+      <span className="loginTitle">Login</span>
+      <form className="loginForm">
+        <label>Email</label>
+        <input
+          className="loginInput"
+          type="text"
+          placeholder="Enter your email..."
+        />
+        <label>Password</label>
+        <input
+          className="loginInput"
+          type="password"
+          placeholder="Enter your password..."
+        />
+        <button className="loginButton">Login</button>
+      </form>
+      <button className="loginRegisterButton">Register</button>
+    </div>
+  );
 }
